6f198c61ead5bb1a381eb5442ea245639f4ef14f
Ersan Karimi <ersanb19@gmail.com>
2022-02-11 14:19:05 +0800

    Update searchBuku.js

--- modules/searchBuku.js
+++ modules/searchBuku.js
@@ -3,11 +3,11 @@ import { makeNewList, rakBukuBelumSelesai, rakBukuSelesai } from './todoList.js'
 import { getDataFromLocalStorage } from './storage.js'
 
 const handleCariBuku = (e) => {
-	const dataTodos = getDataFromLocalStorage()
+	const dataBuku = getDataFromLocalStorage()
 	const regex = new RegExp(e.target.value, 'gi')
-	// filter data dari local storage sesuai dengan inputan pencarian todo
-	const finalData = dataTodos.filter((todo) =>
-		todo.inputBookTitle.match(regex)
+	// filter data dari local storage sesuai dengan inputan pencarian buku
+	const finalData = dataBuku.filter((buku) =>
+		buku.inputBookTitle.match(regex)
 	)
 	makeNewList(finalData)
 }
